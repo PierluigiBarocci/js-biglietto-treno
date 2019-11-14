69efe9aed9b80c602c7c4df7aa3562fbdfc2b78d
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2019-11-14 15:16:07 +0100

    Aggiunte condizioni d'età

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 // variabili
 var distanza = parseInt(prompt('Quanti Km vuoi percorrere?'))
-var prezzoFinale = 0.21 * distanza
+var anni = parseInt(prompt('Quanti anni hai?'))
+var prezzo = 0.21 * distanza
+
+
+// condizioni
+if (anni < 18) {
+    prezzoFinale = prezzo * 0.8
+} else if (anni >= 65) {
+    prezzoFinale = prezzo * 0.6
+}
+else {
+    prezzoFinale = prezzo
+}
 // console
 console.log(distanza);
-console.log(prezzoFinale + '€');
+console.log(anni);
+console.log(prezzo + '€');
+console.log(prezzoFinale + '€')
 // comandi
