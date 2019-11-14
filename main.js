70a5ab256e8df0a7c6818ed479553381d090955d
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2019-11-14 17:14:22 +0100

    Aggiunti i ; (special thanks Luca Formicola)

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
 // variabile distanza
-var distanza = parseInt(prompt('Quanti Km vuoi percorrere?'))
+var distanza = parseInt(prompt('Quanti Km vuoi percorrere?'));
 
 // condizione: l`età viene chiesta solo se la distanza è valida, altrimenti si riparte, è un loop fatto in maniera molto artigianale
 if (isNaN(distanza)) {
     window.location.href = "web_pages/error.html";
 } else {
-    var born = parseInt(prompt('In che anno sei nato?'))
+    var born = parseInt(prompt('In che anno sei nato?'));
 }
 // se l'età non è valida si riparte, altrimenti vengonbo definite le varibili restanti
 if (isNaN(born)) {
     window.location.href = "web_pages/error.html";
 } else {
-    var prezzo = 0.21 * distanza
-    var d = new Date()
-    var year = d.getFullYear()
-    var anni = year - born
+    var prezzo = 0.21 * distanza;
+    var d = new Date();
+    var year = d.getFullYear();
+    var anni = year - born;
 }
 
 // condizioni di sconto del biglietto
 if (anni < 18) {
-    prezzoFinale = prezzo * 0.8
+    prezzoFinale = prezzo * 0.8;
 } else if (anni >= 65) {
-    prezzoFinale = prezzo * 0.6
+    prezzoFinale = prezzo * 0.6;
 }
 else {
-    prezzoFinale = prezzo
+    prezzoFinale = prezzo;
 }
 
 // comandi
