9f682bb12ab49a86f4a7f2007ff221f8254e865d
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2019-11-14 15:06:31 +0100

    Prima variabile con calcolo biglietto

---
+++ main.js
@@ -0,0 +1,7 @@
+// variabili
+var distanza = parseInt(prompt('Quanti Km vuoi percorrere?'))
+var prezzoFinale = 0.21 * distanza
+// console
+console.log(distanza);
+console.log(prezzoFinale + '€');
+// comandi
