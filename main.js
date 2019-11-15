bd174f1db5bb8fa10192d9c2dcdc6d5747e48531
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2019-11-15 11:00:20 +0100

    Esercizio Terminato - Eliminato completamente foglio di stile e corretti i isNaN

--- main.js
+++ main.js
@@ -2,16 +2,16 @@
 var distanza = parseInt(prompt('Quanti Km vuoi percorrere?'));
 
 // condizione: l`età viene chiesta solo se la distanza è valida, altrimenti si riparte, è un loop fatto in maniera molto artigianale
-if (isNaN(distanza)) {
+if (!distanza) {
     window.location.href = "web_pages/error.html";
 } else {
     var born = parseInt(prompt('In che anno sei nato?'));
 }
 // se l'età non è valida si riparte, altrimenti vengonbo definite le varibili restanti
-if (isNaN(born)) {
+if (!born) {
     window.location.href = "web_pages/error.html";
 } else {
-    document.getElementById('showoff').setAttribute("class", "jumbotron jumbotron-fluid bg-dark show");
+    document.getElementById('showoff').setAttribute("class", "jumbotron jumbotron-fluid bg-dark");
     var prezzo = Math.ceil(0.21 * distanza);
     var d = new Date();
     var year = d.getFullYear();
@@ -26,7 +26,7 @@ if (anni < 18) {
 }
 else {
     prezzoFinale = prezzo;
-    document.getElementById('paragWarning').setAttribute("class", "lead text-warning show");
+    document.getElementById('paragWarning').setAttribute("class", "lead text-warning");
 }
 
 // comandi
